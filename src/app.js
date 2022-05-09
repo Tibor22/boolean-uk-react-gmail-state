b697100a4e8e560f99e275367326dbe8b7304859
Tibor22 <tothtibitravel@gmail.com>
2022-05-09 17:58:42 +0100

    extension 3 finished with minor bug

--- src/app.js
+++ src/app.js
@@ -1,47 +1,129 @@
-import Header from './components/header'
+import Header from "./components/header";
+import { useState, useEffect } from "react";
 
-import initialEmails from './data/emails'
+import initialEmails from "./data/emails";
 
-import './styles/app.css'
+import "./styles/app.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
 
+  const [emails, setEmails] = useState(initialEmails);
+  const [filterRead, setFilterRead] = useState();
+  const [readIsChecked, setReadIsChecked] = useState(false);
+  const [filterStarred, setFilterStarred] = useState();
+  const [starredIsChecked, setStarredIsChecked] = useState(false);
+
+  let emailsArr = [];
+  if (readIsChecked) emailsArr = filterRead;
+  if (starredIsChecked) emailsArr = filterStarred;
+  if (starredIsChecked && readIsChecked)
+    emailsArr = emails.filter((email) => {
+      if (email.starred === true && email.read !== true) return email;
+    });
+
+  if (!readIsChecked && !starredIsChecked) emailsArr = emails;
+
+  useEffect(() => {
+    setFilterStarred(emails.filter((email) => email.starred === true));
+  }, []);
+
+  const toggleRead = (email) => {
+    const checkedEmails = emails.map((emailA) => {
+      if (emailA === email) {
+        return { ...emailA, read: !email.read };
+      }
+      return emailA;
+    });
+    setEmails(checkedEmails);
+  };
+  const toggleStar = (email) => {
+    const starredEmails = emails.map((emailA) => {
+      if (emailA === email) {
+        return { ...emailA, starred: !email.starred };
+      }
+      return emailA;
+    });
+    setEmails(starredEmails);
+    setFilterStarred(emails.filter((email) => email.starred === true));
+  };
+
+  const renderUnreadEmails = () => {
+    setFilterRead(emails.filter((email) => email.read !== true));
+    setReadIsChecked(!readIsChecked);
+    console.log(filterRead);
+  };
+
+  const renderStarredEmails = (e) => {
+    setFilterStarred(emails.filter((email) => email.starred === true));
+    setStarredIsChecked(true);
+  };
+
+  console.log("RERENDER:", emailsArr);
   return (
     <div className="app">
       <Header />
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
-            className="item active"
-            // onClick={() => {}}
+            className={`item ${
+              !readIsChecked && !starredIsChecked && "active"
+            }`}
+            onClick={(e) => {
+              setStarredIsChecked(false);
+              return setReadIsChecked(false);
+            }}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{emails.length + 1}</span>
           </li>
           <li
-            className="item"
-            // onClick={() => {}}
+            className={`item starred ${starredIsChecked && "active"}`}
+            id="starred"
+            onClick={renderStarredEmails}
           >
-            <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span>Starred</span>
+            <span className="count">{filterStarred?.length}</span>
           </li>
 
           <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              checked={readIsChecked}
+              onChange={renderUnreadEmails}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {emailsArr &&
+          emailsArr.map((email) => (
+            <li key={email.id} className="email">
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                  checked={email.read}
+                  onChange={() => toggleRead(email)}
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred}
+                  onChange={() => toggleStar(email)}
+                />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
